6259dcf9d14117da3559d2da8872cef1596ef88f
Esteban T <tapiao.e@outlook.com>
2026-03-18 22:37:18 -0700

    GitHub Actions configured

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://estebant96.github.io",
+  base: "/ajolotech",
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
