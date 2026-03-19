95d2d5f8b6762dbc9a8ffefa7ca18f4e128c148f
Esteban T <tapiao.e@outlook.com>
2026-03-18 22:40:16 -0700

    Deploy test

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://estebant96.github.io",
-  base: "/ajolotech",
   vite: {
     plugins: [tailwindcss()],
   },
